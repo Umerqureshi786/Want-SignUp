e92496dc4ddd40b9c7c3bd5b14fd81d0609eadbf
Umar Qureshi <161929656+Umerqureshi786@users.noreply.github.com>
2024-03-08 16:16:46 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+
+var signUp = prompt("Want to Sign Up");
+var students = ["Ali", " Rehan"];
+if(signUp=="yes"){
+    prompt("Enter you name");
+}
+else{
+    alert("Invalid Name");
+}
+
+console.log("array:", students[1]);
